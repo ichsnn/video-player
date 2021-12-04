2aa2b3d50486397c4e970197bc6132599bd8fe1f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-12-04 20:01:30 +0700

    update

--- index.js
+++ index.js
@@ -24,29 +24,78 @@ class Timer {
 const video = document.querySelectorAll('video');
 
 video.forEach((videoPlayer) => {
-    videoPlayer.nextElementSibling.onclick = () => {
-        if (videoPlayer.paused) videoPlayer.play();
-        else videoPlayer.pause();
+    const component = videoPlayer.nextElementSibling;
+    const playControl = component.getElementsByClassName('play-pause')[0];
+    const screenControl = component.getElementsByClassName('screen')[0];
+    const nextControl = component.getElementsByClassName('next-video')[0];
+    const soundControl = component.getElementsByClassName('sound')[0];
+
+    playControl.onclick = (e) => {
+        const play = playControl.getElementsByClassName('play')[0];
+        const pause = playControl.getElementsByClassName('pause')[0];
+        if (videoPlayer.paused) {
+            play.classList.toggle('display-none');
+            pause.classList.toggle('display-none');
+            videoPlayer.play();
+        } else {
+            play.classList.toggle('display-none');
+            pause.classList.toggle('display-none');
+            videoPlayer.pause();
+        }
     };
 
+    soundControl.onmouseenter = (e) => {
+        const slider = soundControl.getElementsByClassName('slider')[0];
+        slider.classList.toggle('display-none');
+    };
+
+    soundControl.onmouseleave = (e) => {
+        const slider = soundControl.getElementsByClassName('slider')[0];
+        slider.classList.toggle('display-none');
+    };
+
+    soundControl.onclick = (e) => {
+        const speaker = soundControl.getElementsByClassName('speaker')[0];
+        const speakerMuted =
+            soundControl.getElementsByClassName('speakerMuted')[0];
+
+        videoPlayer.muted = !videoPlayer.muted;
+
+        speaker.classList.toggle('display-none');
+        speakerMuted.classList.toggle('display-none');
+    };
+
+    screenControl.onclick = (e) => {
+        console.log(videoPlayer.requestFullscreen());
+    };
+
+    component.onmouseenter = (e) => {
+        const controlContain = component.getElementsByClassName('video-controller')[0]
+        component.onmousemove = (e) => {
+            component.style.cursor = 'default'
+            controlContain.classList.remove('display-none')
+        };
+    };
+
+    component.onmouseleave = (e) => {
+        const controlContain = component.getElementsByClassName('video-controller')[0]
+        setTimeout(() => {
+            controlContain.classList.add('display-none')
+            component.style.cursor = 'none'
+        }, 5000)
+    }
+
     videoPlayer.addEventListener('timeupdate', () => {
-        const duration =
-            videoPlayer.nextElementSibling.getElementsByClassName(
-                'duration'
-            )[0];
+        const duration = component.getElementsByClassName('duration')[0];
         const timerDuration = new Timer(videoPlayer.duration);
         duration.innerHTML = timerDuration.getTime();
 
-        const currentTime =
-            videoPlayer.nextElementSibling.getElementsByClassName(
-                'current-time'
-            )[0];
-        
+        const currentTime = component.getElementsByClassName('current-time')[0];
+
         const timerCurrent = new Timer(videoPlayer.currentTime);
         currentTime.innerHTML = timerCurrent.getTime();
 
-        const bar =
-            videoPlayer.nextElementSibling.getElementsByClassName('bar')[0];
+        const bar = component.getElementsByClassName('bar')[0];
 
         bar.setAttribute('role', 'bar');
         bar.setAttribute('aria-valuemin', 0);
